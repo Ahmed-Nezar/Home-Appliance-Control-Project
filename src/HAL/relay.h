c212fa9fcdb7230d813b7c7a6fd29c7f760538a4
Ahmed-Nezar <ahmednezarrr@gmail.com>
2024-12-04 22:09:16 +0200

    feat: adding relay.h module

--- src/HAL/relay.h
+++ src/HAL/relay.h
@@ -1,23 +1,25 @@
 #ifndef RELAY_H
 #define RELAY_H
 
-#include "../utils/types.h"
-#include "../utils/bitwise_operations.h"
-#include "../utils/tm4c123gh6pm.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
 
-#define RELAY_1 0
-#define RELAY_2 1
-
+#define RELAY_PERIPH SYSCTL_PERIPH_GPIOF
+#define RELAY_PORT GPIO_PORTF_BASE
+#define RELAY_1_PIN GPIO_PIN_1
+#define RELAY_2_PIN GPIO_PIN_2
 #define RELAY_ON 1
 #define RELAY_OFF 0
+#define RELAY_1 1
+#define RELAY_2 2
 
-#define RELAY_1_PORT PORTB
-#define RELAY_1_PIN PIN1
-
-#define RELAY_2_PORT PORTB
-#define RELAY_2_PIN PIN2
 
 void relay_init(void);
-void relay_control(uint8 relay, uint8 state);
+void relay_control(uint8_t relay, uint8_t state);
 
 #endif
